c72e1b758fe005965908e73cd7c121125ce9c054
SaKa <karta1119371@gmail.com>
2020-06-17 14:30:30 +0800

    feat: 🎸 Add focal loss

--- methods/ArcFace.py
+++ methods/ArcFace.py
@@ -8,8 +8,10 @@ from torch.autograd import Variable
 import numpy as np
 import torch.nn.functional as F
 
+from loss.focal_loss import FocalLoss
+
 class ArcFaceTrain(nn.Module):
-    def __init__(self, model_func, num_class, s=10.0, m=0.50, easy_margin=False, pretrain=False):
+    def __init__(self, model_func, num_class, s=30.0, m=0.50, easy_margin=False, pretrain=False):
         super(ArcFaceTrain, self).__init__()
         self.feature    = model_func()
         self.s = s
@@ -28,7 +30,7 @@ class ArcFaceTrain(nn.Module):
         self.classifier.bias.data.fill_(0)
 
         self.num_class = num_class
-        self.loss_fn = nn.CrossEntropyLoss()
+        self.loss_fn = FocalLoss() # nn.CrossEntropyLoss()
         self.top1 = utils.AverageMeter()
 
     def forward(self,x):
@@ -46,16 +48,17 @@ class ArcFaceTrain(nn.Module):
         y = Variable(y.cuda())
 
         feat = self.forward_feature(x)
-        cosine = F.linear(F.normalize(feat + 1e-9), F.normalize(self.weight + 1e-9))
+        cosine = F.linear(F.normalize(feat), F.normalize(self.weight))
         sine = torch.sqrt((1.0 - torch.pow(cosine, 2) + 1e-9).clamp(0, 1))
-        phi = cosine * self.cos_m - sine * self.sin_m * self.s + 1e-9
+        phi = cosine * self.cos_m - sine * self.sin_m
         if self.easy_margin:
             phi = torch.where(cosine > 0, phi, cosine)
         else:
-            phi = torch.where(cosine > self.th, phi, (cosine - self.mm) * self.s)
+            phi = torch.where(cosine > self.th, phi, (cosine - self.mm))
         one_hot = torch.zeros(cosine.size(), device='cuda')
         one_hot.scatter_(1, y.view(-1, 1).long(), 1)
-        scores = (one_hot * phi) + ((1.0 - one_hot) * cosine * self.s) + 1e-9
+        scores = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        scores *= self.s
 
         _, predicted = torch.max(scores.data, 1)
         correct = predicted.eq(y.data).cpu().sum()
